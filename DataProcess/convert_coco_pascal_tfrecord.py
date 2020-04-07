70cd0df56ba273184a6e2aa44fdb9637d3d1f0d7
alexchung <yonganzhong@outlook.com>
2020-04-07 17:52:21 +0800

    use integer method by int(np.rint()) add vie bar process visual

--- DataProcess/convert_coco_pascal_tfrecord.py
+++ DataProcess/convert_coco_pascal_tfrecord.py
@@ -14,12 +14,14 @@ import tensorflow  as tf
 import xml.etree.cElementTree as ET
 import cv2 as cv
 
+from Util.tools import view_bar, makedir
+
 # original_dataset_dir = 'F:/datasets/Pascal VOC 2012/VOCdevkit/VOC2012'
 
-pascal_dataset_dir = '/home/alex/Documents/datasets/Pascal_VOC_2012/VOCtrainval/VOCdevkit_test'
+pascal_dataset_dir = '/home/alex/Documents/dataset/Pascal_VOC_2012/VOCtrainval/VOCdevkit_test'
 pascal_tfrecord_dir = os.path.join(pascal_dataset_dir, 'tfrecords')
 
-coco_dataset_dir = '/home/alex/Documents/dataset/COCO_2017/sub_coco'
+coco_dataset_dir = '/home/alex/Documents/dataset/COCO_2017/new_sub_coco'
 coco_tfrecord_dir = os.path.join(coco_dataset_dir, 'tfrecords')
 
 NAME_LABEL_MAP = {
@@ -50,26 +52,13 @@ NAME_LABEL_MAP = {
 tf.app.flags.DEFINE_string('dataset_dir', pascal_dataset_dir, 'Voc dir')
 tf.app.flags.DEFINE_string('xml_dir', 'Annotations', 'xml dir')
 tf.app.flags.DEFINE_string('image_dir', 'JPEGImages', 'image dir')
+tf.app.flags.DEFINE_string('save_dir', pascal_tfrecord_dir, 'save directory')
 tf.app.flags.DEFINE_string('save_name', 'train', 'save name')
-tf.app.flags.DEFINE_string('save_dir', pascal_tfrecord_dir, 'save name')
 tf.app.flags.DEFINE_string('img_format', '.jpg', 'format of image')
 
 
 FLAGS = tf.app.flags.FLAGS
 
-def makedir(path):
-    """
-    create dir
-    :param path:
-    :return:
-    """
-    a = os.path.exists(path)
-    if os.path.exists(path) is False:
-        try:
-            os.makedirs(path)
-            print('{0} has been created'.format(path))
-        except Exception as e:
-            print(e)
 
 # ------------------------------------------convert pascal to tfrecord-----------------------------------------------
 def convert_pascal_to_tfrecord(image_path, xml_path, save_path):
@@ -79,12 +68,13 @@ def convert_pascal_to_tfrecord(image_path, xml_path, save_path):
     :param annotation_dir:
     :return: None
     """
-    # record_file = os.path.join(FLAGS.save_dir, FLAGS.save_name+'.tfrecord')
     makedir(save_path)
-    write = tf.io.TFRecordWriter(save_path)
+    record_path = os.path.join(save_path, FLAGS.save_name+'.record')
+    write = tf.io.TFRecordWriter(record_path)
 
     num_samples = 0
-    for n, xml in enumerate(glob.glob(os.path.join(xml_path, '*.xml'))):
+    sample_xml = glob.glob(os.path.join(xml_path, '*.xml'))
+    for n, xml in enumerate(sample_xml):
         img_name = os.path.basename(xml).split('.')[0] + FLAGS.img_format
         img_path = os.path.join(image_path, img_name)
         if not os.path.exists(img_path):
@@ -100,6 +90,9 @@ def convert_pascal_to_tfrecord(image_path, xml_path, save_path):
             image_record = serialize_example(image=rgb_image, img_height=img_height, img_width=img_width, img_depth=3,
                                              filename=img_name, gtbox_label=gtbox_label)
             write.write(record=image_record)
+
+            view_bar(message='\nConversion progress', num=num_samples, total=len(sample_xml))
+            num_samples += 1
             num_samples += 1
         except Exception as e:
             print(e)
@@ -112,7 +105,9 @@ def read_xml_gtbox_and_label(xml_path):
     read gtbox(ground truth) and label from xml
     :param xml_path: the path of voc xml
     :return: a list contains gtboxes and labels, shape is [num_of_gtboxes, 5],
-           and has [xmin, ymin, xmax, ymax, label] in a per row
+           and has [xmin, ymin, xmax, ymax, label](top_left, down_right) in a per row
+
+
     """
     tree = ET.parse(xml_path)
     root = tree.getroot()
@@ -123,9 +118,9 @@ def read_xml_gtbox_and_label(xml_path):
         if child_of_root.tag == 'size':
             for child_item in child_of_root:
                 if child_item.tag == 'width':
-                    img_width = int(child_item.text)
+                    img_width = int(np.rint(float(child_item.text)))
                 if child_item.tag == 'height':
-                    img_height = int(child_item.text)
+                    img_height = int(np.rint(float(child_item.text)))
 
         if child_of_root.tag == 'object':
             label = None
@@ -134,16 +129,16 @@ def read_xml_gtbox_and_label(xml_path):
                     # label = NAME_LABEL_MAP[child_item.text]
                     label=NAME_LABEL_MAP[child_item.text]
                 if child_item.tag == 'bndbox':
-                    tmp_box = []
+
                     for node in child_item:
                         if node.tag == 'xmin':
-                            xmin = int(eval(node.text))
+                            xmin = int(np.rint(float(node.text)))
                         if node.tag == 'ymin':
-                            ymin = int(eval(node.text))
+                            ymin = int(np.rint(float(node.text)))
                         if node.tag == 'xmax':
-                            xmax = int(eval(node.text))
+                            xmax = int(np.rint(float(node.text)))
                         if node.tag == 'ymax':
-                            ymax = int(eval(node.text))
+                            ymax = int(np.rint(float(node.text)))
                     tmp_box = [xmin, ymin, xmax, ymax]
                     # tmp_box.append()
                     assert label is not None, 'label is none, error'
@@ -152,15 +147,15 @@ def read_xml_gtbox_and_label(xml_path):
 
     gtbox_label = np.array(box_list, dtype=np.int32)  # [x1, y1. x2, y2, label]
 
-    xmin, ymin, xmax, ymax, label = gtbox_label[:, 0], gtbox_label[:, 1], gtbox_label[:, 2], gtbox_label[:, 3], \
-                                    gtbox_label[:, 4]
-    gtbox_label = np.transpose(np.stack([ymin, xmin, ymax, xmax, label], axis=0))  # [ymin, xmin, ymax, xmax, label]
+    # xmin, ymin, xmax, ymax, label = gtbox_label[:, 0], gtbox_label[:, 1], gtbox_label[:, 2], gtbox_label[:, 3], \
+    #                                 gtbox_label[:, 4]
+    # gtbox_label = np.transpose(np.stack([ymin, xmin, ymax, xmax, label], axis=0))  # [ymin, xmin, ymax, xmax, label]
 
     return img_height, img_width, gtbox_label
 
 
 # --------------------------------------------convert coco to tfrecord------------------------------------------------
-def convert_coco_to_tfrecord(src_path, save_path):
+def convert_coco_to_tfrecord(src_path, save_path, raw_coco=True):
     """
 
     :param src_path:
@@ -176,18 +171,21 @@ def convert_coco_to_tfrecord(src_path, save_path):
 
     annotation_list = glob.glob(os.path.join(anns_path, '*.json'))
 
-    num_samples = 0
-
 
     anns, cats, imgs, img_anns, cate_imgs = create_index(annotation_list[0])
 
+    num_samples = 0
     for img_id, img_annotations in img_anns.items():
 
         # get gtbox_label
         gtbox_label = read_json_gtbox_label(img_annotations)
 
-        # get image
-        img_name = '0'*(12 - len(str(img_id))) + '{0}.jpg'.format(img_id)
+        # get image name
+        if raw_coco:
+            img_name = '0'*(12 - len(str(img_id))) + '{0}.jpg'.format(img_id)
+        else:
+            img_name = '{0}.jpg'.format(img_id)
+
         img_path = os.path.join(imgs_path, img_name)
 
         try:
@@ -198,9 +196,10 @@ def convert_coco_to_tfrecord(src_path, save_path):
             img_width = rgb_image.shape[1]
 
             image_record = serialize_example(image=rgb_image, img_height=img_height, img_width=img_width,
-                                             img_depth=3,
-                                             filename=img_name, gtbox_label=gtbox_label)
+                                             img_depth=3, filename=img_name, gtbox_label=gtbox_label)
             write.write(record=image_record)
+
+            view_bar(message='\nConversion progress', num=num_samples, total=len(img_anns))
             num_samples += 1
 
         except Exception as e:
@@ -246,12 +245,24 @@ def read_json_gtbox_label(img_anns):
     :param img_id:
     :return:
     """
-    gtbox_label = np.zeros((0, 5))
+    # gtbox_label = np.zeros((0, 5), dtype=tf.float32)
+    gtbox_label = None
     for annotation in img_anns:
         bbox = annotation['bbox']
+
+        x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
+        # (x,y, w, h) -> (x_min, y_min, x_max, y_max)
+        x_min, y_min, x_max, y_max = int(np.rint(x)), int(np.rint(y)), int(np.rint(x + w)), int(np.rint(y + h))
+        new_bbox = [x_min, y_min, x_max, y_max]
         label = annotation['category_id']
-        bbox.append(label)
-        gtbox_label = np.vstack((gtbox_label, bbox))
+        new_bbox.append(label)
+
+        new_bbox = np.expand_dims(new_bbox, axis=0)
+        # convert (x, y, w, h) to (x_min, y_min, x_max, y_max)
+        if gtbox_label is None:
+            gtbox_label = new_bbox
+        else:
+            gtbox_label = np.vstack((gtbox_label, new_bbox))
 
     return gtbox_label
 
@@ -266,12 +277,12 @@ def serialize_example(image, img_height, img_width, img_depth, filename, gtbox_l
     # create a dict mapping the feature name to the tf.Example compatible
     # image_shape = tf.image.decode_jpeg(image_string).eval().shape
     feature = {
-        'image': _bytes_feature(image.tostring()),
+        'image': _bytes_feature(image.tobytes()),
         'height': _int64_feature(img_height),
         'width': _int64_feature(img_width),
         'depth':_int64_feature(img_depth),
         'filename': _bytes_feature(filename.encode()),
-        'gtboxes_and_label': _bytes_feature(gtbox_label.tostring()),
+        'gtboxes_and_label': _bytes_feature(gtbox_label.tobytes()),
         'num_objects': _int64_feature(gtbox_label.shape[0])
     }
     # create a feature message using tf.train.Example
@@ -287,16 +298,19 @@ def _bytes_feature(value):
 
 
 if __name__ == "__main__":
+    # # test xml
     # image_path = os.path.join(FLAGS.dataset_dir, FLAGS.image_dir)
     # xml_path = os.path.join(FLAGS.dataset_dir, FLAGS.xml_dir)
-    # save_path = os.path.join(FLAGS.save_dir, FLAGS.save_name + '.record')
+    # save_path = FLAGS.save_dir
     #
     # convert_pascal_to_tfrecord(image_path=image_path, xml_path=xml_path, save_path=save_path)
 
+
+    # test json
     img_path = os.path.join(coco_dataset_dir, 'Images')
     ann_path = os.path.join(coco_dataset_dir, 'Annotations')
 
-    convert_coco_to_tfrecord(src_path=coco_dataset_dir, save_path=coco_tfrecord_dir)
+    convert_coco_to_tfrecord(src_path=coco_dataset_dir, save_path=coco_tfrecord_dir, raw_coco=False)
 
 
 
